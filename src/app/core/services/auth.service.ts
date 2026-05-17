0ed076855dfce27661585a5010cf2dbd5da88c8d
skulltom666 <skulltom666@outlook.com>
2026-05-17 18:49:11 -0500

    fix 1.5

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthResponse } from '../../interfaces/auth-response.model';
+import { environment } from '../../../enviroment/environment';
 // Si el servicio está en una carpeta 'services', usa '../'
 // Si el servicio está suelto en 'app', usa './'
 
@@ -9,15 +10,19 @@ import { AuthResponse } from '../../interfaces/auth-response.model';
   providedIn: 'root',
 })
 export class AuthService {
-  private API_URL = 'http://localhost:8080/api/auth';
+  private apiUrl = environment.apiUrl;
+
+  registro(data: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/api/auth/registro`, data);
+  }
 
   constructor(private http: HttpClient) {}
 
   login(credenciales: any): Observable<AuthResponse> {
-    return this.http.post<AuthResponse>(`${this.API_URL}/login`, credenciales);
+    return this.http.post<AuthResponse>(`${this.apiUrl}/login`, credenciales);
   }
 
   registrar(usuario: any): Observable<AuthResponse> {
-    return this.http.post<AuthResponse>(`${this.API_URL}/registro`, usuario);
+    return this.http.post<AuthResponse>(`${this.apiUrl}/registro`, usuario);
   }
 }
